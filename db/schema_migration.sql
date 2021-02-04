91b862861f98c809c11a8e6af838fffec8a0b482
Vivian Stanley <vivianstanley@Vivians-MacBook-Air.local>
2021-02-03 20:21:28 -0800

    commit tree bf2a7c5d505bda12e699728ab72730b39f04c0b6

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,2 +1,6 @@
 -- Write your sql commands to create your db schema here.
+
+CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT);
 -- It should create a table in a new table in the db/ directory.
+
+ALTER TABLE pokemon ADD hp INTEGER NOT NULL DEFAULT(60);
